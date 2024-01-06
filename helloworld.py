2ea81c81f429da4b15321e638f6887ddb5e9720f
Daksh <dakshtyagi94@gmail.com>
2024-01-06 05:57:53 +0000

    Add custom statements to handle different input values

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
 i = int(input("How many hellos are too many hellos? "))
+if i == 1:
+    print("Aww that's so sad, you don't want to say hello?")
+if i <= 0:
+    print("You're not making any sense!")
 for i in range(i-1):
     print("Hello World!")
